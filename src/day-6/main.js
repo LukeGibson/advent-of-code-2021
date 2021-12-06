22d96cc34693ed1d4553b4748a647c98e22bc28f
Luke <luke.gibson98@hotmail.com>
2021-12-06 12:29:09 +0000

    day 6 clean

--- src/day-6/main.js
+++ src/day-6/main.js
@@ -2,15 +2,14 @@ import fs from 'fs';
 
 const text = fs.readFileSync('./input.txt', 'utf-8').split(',');
 const input = text.map(num => Number(num));
-const dayLimit = 256;
 
 let fishCounts = new Array(9).fill(0);
 input.forEach(fish => fishCounts[fish] += 1);
 
-for (let day=0; day<dayLimit; day++) {
+for (let day=0; day<256; day++) {
   let newFishCounts = new Array(9).fill(0);
 
-  for (let i=8; i>0; i--) newFishCounts[i-1] = fishCounts[i]
+  for (let i=8; i>0; i--) newFishCounts[i-1] = fishCounts[i];
 
   newFishCounts[6] += fishCounts[0];
   newFishCounts[8] += fishCounts[0];
